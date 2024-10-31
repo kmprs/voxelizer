de83ea435bc9f18742841fea8c0339e2606db07e
Jakob Kampers <jakobkampers23@gmail.com>
2024-10-31 18:10:20 +0100

    refactor: splitted gui code up in different methods

--- src/gui.cpp
+++ src/gui.cpp
@@ -4,138 +4,6 @@
 extern std::shared_ptr<DataHandler> dataHandler;
 static int resolution = INIT_RESOLUTION;
 
-GUI::GUI()
-{
-}
-
-
-void GUI::createFrame( float width, float height, int x, int y )
-{
-    ImGui_ImplSDL2_NewFrame();
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui::NewFrame();
-
-    // Minimal window flags: No title bar, no resizing, no moving
-    ImGuiWindowFlags window_flags =
-            ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-            ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-
-    // LEFT FRAME
-    ImGui::SetNextWindowPos( ImVec2( static_cast<float>(x), static_cast<float>(y)));
-    ImGui::SetNextWindowSize( ImVec2( width, height ));
-
-    ImGui::Begin( "Left GUI", nullptr, window_flags );
-
-    // REPRESENTATION
-    const char* currentRepresentation = ( dataHandler->getModelRepresentation() == VOXEL )
-                                        ? "Voxel" : "Triangle";
-    float buttonWidth = width - 20.f;
-    ImGui::Text( "Triangle/Voxel" );
-    if ( ImGui::Button( currentRepresentation, ImVec2( buttonWidth, 0 )))
-    {
-        dataHandler->toggleModelRepresentation();
-    }
-    ImGui::Spacing();
-
-    // ALGORITHM SELECTION
-    static bool isVoxelizationMenuOpen = false;
-    const char* labels[] = { "Optimized Voxelizer", "Naive Voxelizer", "Octree Voxelizer", "BVH Voxelizer" };
-
-    int selectedIndex = static_cast<int>(dataHandler->getVoxelizationAlgorithm());
-    std::string title = std::string(labels[selectedIndex]) + " ";
-
-    ImGui::Text("Voxelization Method");
-    ImGui::SetNextItemOpen(isVoxelizationMenuOpen);
-
-    if (ImGui::CollapsingHeader(title.c_str()))
-    {
-        isVoxelizationMenuOpen = true;
-
-        for (int i = 0; i < 4; ++i)
-        {
-            if (ImGui::Selectable(labels[i], selectedIndex == i))
-            {
-                dataHandler->setAlgorithm(static_cast<VoxelizationAlgorithm>(i));
-                isVoxelizationMenuOpen = false;
-                break;
-            }
-        }
-    }
-    ImGui::Spacing();
-
-    // VOXEL RESOLUTION
-    ImGui::Text( "Voxel Resolution" );
-    ImGui::PushItemWidth( buttonWidth );
-    if ( ImGui::SliderInt( "", &resolution, 1, 10 ))
-    {};
-    if ( ImGui::IsItemDeactivatedAfterEdit())
-    {
-        dataHandler->setVoxelResolution( resolution );
-    }
-    ImGui::Spacing();
-
-    // VOXEL COLOR
-    glm::vec3 voxelColor = dataHandler->getVoxelColor();
-    float rgb[3] = { voxelColor.x, voxelColor.y, voxelColor.z };
-    ImGui::Text( "Voxel Color" );
-    ImGui::ColorEdit3( "##hiddenLabel", rgb );  // Simplified color picker
-    dataHandler->setVoxelColor( { rgb[0], rgb[1], rgb[2] } );
-    ImGui::Spacing();
-
-    // CAMERA MODE
-    const char* cameraMode = ( dataHandler->getCameraMode() == CENTERED )
-                             ? "centered" : "free";
-    ImGui::Text( "Camera Mode" );
-    if ( ImGui::Button( cameraMode, ImVec2( buttonWidth, 0 )))
-    {
-        dataHandler->toggleCameraMode();
-    }
-    ImGui::Spacing();
-
-    // CAMERA SPEED
-    float speed = dataHandler->getCameraSpeed();
-    ImGui::Text( "Camera Speed" );
-    ImGui::InputFloat( "##camera speed", &speed, 1.f, 1.f, "%.1f" );
-    ImGui::Spacing();
-    dataHandler->setCameraSpeed( speed );
-
-    speed = dataHandler->getRotationSpeed();
-    ImGui::Text( "Rotation Speed" );
-    ImGui::InputFloat( "##rotation speed", &speed, 10.f, 10.f, "%.1f" );
-    ImGui::Spacing();
-    dataHandler->setRotationSpeed( speed );
-
-
-    ImGui::End();
-
-    // RIGHT FRAME
-    ImGui::SetNextWindowPos(
-            ImVec2( static_cast<float>(dataHandler->getWindowWidth()) - width,
-                    static_cast<float>(y)));
-    ImGui::SetNextWindowSize( ImVec2( width, height ));
-    ImGui::Begin( "Right GUI", nullptr, window_flags );
-    ImGui::End();
-
-    // BOTTOM BAR - PERFORMANCE DATA
-    if ( !dataHandler->isWindowFreezed())
-    {
-        float performanceHeight = 30.f;
-        ImGui::SetNextWindowPos( ImVec2( width,
-                                         static_cast<float>(dataHandler->getWindowHeight()) -
-                                         performanceHeight ));
-        ImGui::SetNextWindowSize(
-                ImVec2( static_cast<float>(dataHandler->getWindowWidth()) - 2 * width,
-                        performanceHeight ));
-        ImGui::Begin( "Performance stats", nullptr,
-                      window_flags | ImGuiWindowFlags_NoBackground );
-        ImGui::Text( "%d FPS", dataHandler->getCurrentFPS());
-        ImGui::SameLine();
-        ImGui::Text( "%d voxels", dataHandler->getNumberOfVoxels());
-
-        ImGui::End();
-    }
-}
-
 void GUI::setStyles()
 {
     ImGuiIO &io = ImGui::GetIO();
@@ -169,3 +37,171 @@ void GUI::setStyles()
 
     ImGui::GetIO().FontGlobalScale = 1.f;
 }
+
+void GUI::createFrame( float width, float height, int x, int y )
+{
+    ImGui_ImplSDL2_NewFrame();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+
+    // Minimal window flags: No title bar, no resizing, no moving
+    ImGuiWindowFlags window_flags =
+            ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    float buttonWidth = width - 20;
+
+    // LEFT FRAME
+    ImGui::SetNextWindowPos( ImVec2( static_cast<float>(x), static_cast<float>(y)));
+    ImGui::SetNextWindowSize( ImVec2( width, height ));
+    ImGui::Begin( "Left GUI", nullptr, window_flags );
+
+    buttonRepresentation( buttonWidth );
+    ImGui::Spacing();
+
+    collapseAlgorithmSelection();
+    ImGui::Spacing();
+
+    sliderVoxelResolution( buttonWidth );
+    ImGui::Spacing();
+
+    colorPickerVoxel();
+    ImGui::Spacing();
+
+    buttonCameraMode( buttonWidth );
+    ImGui::Spacing();
+
+    numberInputCameraSpeed();
+    ImGui::Spacing();
+
+    numberInputRotationSpeed();
+    ImGui::Spacing();
+
+    ImGui::End();
+
+    // RIGHT FRAME
+    ImGui::SetNextWindowPos(
+            ImVec2( static_cast<float>(dataHandler->getWindowWidth()) - width,
+                    static_cast<float>(y)));
+    ImGui::SetNextWindowSize( ImVec2( width, height ));
+    ImGui::Begin( "Right GUI", nullptr, window_flags );
+    ImGui::End();
+
+    // BOTTOM BAR - PERFORMANCE DATA
+    if ( !dataHandler->isWindowFreezed())
+    {
+        float performanceHeight = 30.f;
+        ImGui::SetNextWindowPos( ImVec2( width,
+                                         static_cast<float>(dataHandler->getWindowHeight()) -
+                                         performanceHeight ));
+        ImGui::SetNextWindowSize(
+                ImVec2( static_cast<float>(dataHandler->getWindowWidth()) - 2 * width,
+                        performanceHeight ));
+        ImGui::Begin( "Performance stats", nullptr,
+                      window_flags | ImGuiWindowFlags_NoBackground );
+
+        showPerformanceData();
+
+        ImGui::End();
+    }
+}
+
+void GUI::buttonRepresentation( float buttonWidth )
+{
+    const char* currentRepresentation = ( dataHandler->getModelRepresentation() == VOXEL )
+                                        ? "Voxel" : "Triangle";
+    ImGui::Text( "Triangle/Voxel" );
+    if ( ImGui::Button( currentRepresentation, ImVec2( buttonWidth, 0 )))
+    {
+        dataHandler->toggleModelRepresentation();
+    }
+}
+
+void GUI::collapseAlgorithmSelection()
+{
+// ALGORITHM SELECTION
+    static bool isVoxelizationMenuOpen = false;
+    const char* labels[] = { "Optimized Voxelizer", "Naive Voxelizer", "Octree Voxelizer",
+                             "BVH Voxelizer" };
+
+    int selectedIndex = static_cast<int>(dataHandler->getVoxelizationAlgorithm());
+    std::string title = std::string( labels[selectedIndex] ) + " ";
+
+    ImGui::Text( "Voxelization Method" );
+    ImGui::SetNextItemOpen( isVoxelizationMenuOpen );
+
+    if ( ImGui::CollapsingHeader( title.c_str()))
+    {
+        isVoxelizationMenuOpen = true;
+
+        for ( int i = 0; i < 4; ++i )
+        {
+            if ( ImGui::Selectable( labels[i], selectedIndex == i ))
+            {
+                dataHandler->setAlgorithm( static_cast<VoxelizationAlgorithm>(i));
+                isVoxelizationMenuOpen = false;
+                break;
+            }
+        }
+    }
+}
+
+void GUI::sliderVoxelResolution( float buttonWidth )
+{
+    // VOXEL RESOLUTION
+    ImGui::Text( "Voxel Resolution" );
+    ImGui::PushItemWidth( buttonWidth );
+    if ( ImGui::SliderInt( "", &resolution, 1, 10 ))
+    {};
+    if ( ImGui::IsItemDeactivatedAfterEdit())
+    {
+        dataHandler->setVoxelResolution( resolution );
+    }
+}
+
+void GUI::colorPickerVoxel()
+{
+    glm::vec3 voxelColor = dataHandler->getVoxelColor();
+    float rgb[3] = { voxelColor.x, voxelColor.y, voxelColor.z };
+    ImGui::Text( "Voxel Color" );
+    ImGui::ColorEdit3( "", rgb );
+    dataHandler->setVoxelColor( { rgb[0], rgb[1], rgb[2] } );
+}
+
+void GUI::buttonCameraMode( float buttonWidth )
+{
+    const char* cameraMode = ( dataHandler->getCameraMode() == CENTERED )
+                             ? "centered" : "free";
+    ImGui::Text( "Camera Mode" );
+    if ( ImGui::Button( cameraMode, ImVec2( buttonWidth, 0 )))
+    {
+        dataHandler->toggleCameraMode();
+    }
+}
+
+void GUI::numberInputCameraSpeed()
+{
+    float speed = dataHandler->getCameraSpeed();
+    ImGui::Text( "Camera Speed" );
+    ImGui::InputFloat( "##camera speed", &speed, 1.f, 1.f, "%.1f" );
+    ImGui::Spacing();
+    dataHandler->setCameraSpeed( speed );
+}
+
+void GUI::numberInputRotationSpeed()
+{
+    static float speed = dataHandler->getRotationSpeed();
+    ImGui::Text( "Rotation Speed" );
+    ImGui::InputFloat( "##rotation_speed", &speed, 10.f, 10.f, "%.1f" );
+    ImGui::Spacing();
+    dataHandler->setRotationSpeed( speed );
+}
+
+void GUI::showPerformanceData()
+{
+    ImGui::Text( "%d FPS", dataHandler->getCurrentFPS());
+    ImGui::SameLine();
+    ImGui::Text( "%d voxels", dataHandler->getNumberOfVoxels());
+}
+
+
+
--- src/gui.hpp
+++ src/gui.hpp
@@ -12,10 +12,19 @@
 class GUI
 {
 public:
-    GUI();
+    GUI() = default;
     static void createFrame( float width, float height, int x, int y );
     static void setStyles();
 
 
 private:
+    static void buttonRepresentation( float buttonWidth );
+    static void collapseAlgorithmSelection();
+    static void sliderVoxelResolution( float buttonWidth );
+    static void colorPickerVoxel();
+    static void buttonCameraMode( float buttonWidth );
+    static void numberInputCameraSpeed();
+    static void numberInputRotationSpeed();
+    static void showPerformanceData();
+
 };
